f971d4ea6c0d645e637d08a6e110871d3d9fe123
laylin41 <yovich2006@gmail.com>
2025-05-15 22:01:26 +0300

    i hate this

--- OnlineShopApp/OnlineShopApp/asgi.py
+++ OnlineShopApp/OnlineShopApp/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineShopApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineShopApp.OnlineShopApp.settings')
 
 application = get_asgi_application()
